cd09fca919906c841dd102911f4efa9a0b661139
조병우 <whquddn55@gmail.com>
2024-05-07 01:40:44 +0900

    chore: 패키지 초기 설정

--- packages/config-eslint/index.js
+++ packages/config-eslint/index.js
@@ -1,19 +1,40 @@
 module.exports = {
-  env: {
-    node: true,
-  },
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  plugins: ["@typescript-eslint"],
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "@typescript-eslint/no-non-null-assertion": "off",
-  },
+	root: true,
+	globals: {
+		React: true,
+		JSX: true,
+	},
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	ignorePatterns: ["node_modules/", "dist/", "public/"],
+	plugins: ["react", "jsx-a11y", "react-hooks", "react-refresh"],
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:jsx-a11y/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+		"eslint-config-turbo",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+	},
+	rules: {
+		eqeqeq: "warn",
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": "warn",
+		"no-var": "warn",
+		"require-await": "warn",
+		"react/no-array-index-key": "warn",
+	},
 };
