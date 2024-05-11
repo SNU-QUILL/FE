33747d9efced50feb0e33453856933c9568c8725
조병우 <whquddn55@gmail.com>
2024-05-11 15:40:32 +0900

    feature: 공통 eslint 설정

--- packages/config-eslint/index.js
+++ packages/config-eslint/index.js
@@ -1,40 +1,34 @@
 module.exports = {
-	root: true,
-	globals: {
-		React: true,
-		JSX: true,
-	},
-	env: {
-		browser: true,
-		es6: true,
-		node: true,
-	},
-	ignorePatterns: ["node_modules/", "dist/", "public/"],
-	plugins: ["react", "jsx-a11y", "react-hooks", "react-refresh"],
-	extends: [
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:react/jsx-runtime",
-		"plugin:jsx-a11y/recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:@typescript-eslint/recommended",
-		"prettier",
-		"eslint-config-turbo",
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		sourceType: "module",
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: "latest",
-	},
-	rules: {
-		eqeqeq: "warn",
-		"no-unused-vars": "off",
-		"@typescript-eslint/no-unused-vars": "warn",
-		"no-var": "warn",
-		"require-await": "warn",
-		"react/no-array-index-key": "warn",
-	},
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  ignorePatterns: ["node_modules/", "dist/", "public/"],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prefer-stateless-function": "off",
+    "react/jsx-filename-extension": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "no-nested-ternary": "off", // 중첩된 삼항 연산자 허용
+    "react/jsx-props-no-spreading": "off", // props에 spread 사용 가능
+    "import/no-unresolved": "off", // import 경로 오류 허용(절대 경로 사용)
+    "import/extensions": "off", // import 확장자 오류 허용(절대 경로 사용)
+  },
 };
