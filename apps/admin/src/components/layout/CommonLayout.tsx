8125d27b0e6ba2229f9bb133f13643332b9aaa16
조병우 <whquddn55@gmail.com>
2024-05-15 22:31:22 +0900

    feature: 공통 Layout에서 Tab을 변수로 관리

--- apps/admin/src/components/layout/CommonLayout.tsx
+++ apps/admin/src/components/layout/CommonLayout.tsx
@@ -1,4 +1,5 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useMemo } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 const CommonLayout = () => {
@@ -6,23 +7,40 @@ const CommonLayout = () => {
   const location = useLocation();
   const currentPath = location.pathname.split("/").pop() || "";
 
+  const tabItems = useMemo(() => {
+    return [
+      {
+        label: "Main",
+        value: "main",
+      },
+      {
+        label: "User",
+        value: "user",
+      },
+      {
+        label: "Logout",
+        value: "logout",
+      },
+    ];
+  }, []);
+
   return (
     <Tabs
       defaultValue={currentPath}
-      className='w-full h-full flex pl-10'
+      className='w-full h-full flex'
       orientation='vertical'
       onValueChange={value => navigate(`/${value}`)}
     >
-      <TabsList className='h-full flex-col gap-3 border-r border-primary rounded-none bg-white'>
-        <TabsTrigger value='main' className='data-[state=active]:shadow-primary'>
-          Main
-        </TabsTrigger>
-        <TabsTrigger value='user' className='data-[state=active]:shadow-primary'>
-          User
-        </TabsTrigger>
-        <TabsTrigger value='logout' className='data-[state=active]:shadow-primary'>
-          Logout
-        </TabsTrigger>
+      <TabsList className='h-full w-40 flex-col gap-3 border-r border-secondary rounded-none bg-white'>
+        {tabItems.map(item => (
+          <TabsTrigger
+            key={item.label}
+            value={item.value}
+            className='w-full justify-start data-[state=active]:bg-primary data-[state=active]:text-secondary'
+          >
+            {item.label}
+          </TabsTrigger>
+        ))}
       </TabsList>
       <div className='p-5'>
         <Outlet />
