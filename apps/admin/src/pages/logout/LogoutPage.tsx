d7b8ddf5f4534dbefd484747d6ea7edc3d3dd22f
조병우 <whquddn55@gmail.com>
2024-05-18 18:10:43 +0900

    feature: 로그아웃 기능 추가

--- apps/admin/src/pages/logout/LogoutPage.tsx
+++ apps/admin/src/pages/logout/LogoutPage.tsx
@@ -1,4 +1,27 @@
+import { useAuthStore } from "@/stores/authStore";
+import { useGlobalConfirmStore } from "@/stores/globalConfirm";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const LogoutPage = () => {
+  const { openDialog, closeDialog } = useGlobalConfirmStore();
+  const { setAccessToken, setRefreshToken } = useAuthStore();
+  const navigate = useNavigate();
+  useEffect(() => {
+    openDialog({
+      title: "Logout",
+      description: "Are you sure you want to logout?",
+      onConfirm: () => {
+        closeDialog();
+        setAccessToken("");
+        setRefreshToken("");
+      },
+      onCancel: () => {
+        closeDialog();
+        navigate("/main", { replace: true });
+      },
+    });
+  }, []);
   return <div>LogoutPage</div>;
 };
 export default LogoutPage;
