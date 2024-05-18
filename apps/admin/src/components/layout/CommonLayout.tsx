edbb88faf4e4f94be85343fa64bd7368d3d7ca0e
조병우 <whquddn55@gmail.com>
2024-05-18 17:43:01 +0900

    feature: 새로고침 시 CommonLayout에 라우팅이 적용되도록 변경

--- apps/admin/src/components/layout/CommonLayout.tsx
+++ apps/admin/src/components/layout/CommonLayout.tsx
@@ -5,23 +5,25 @@ import { Outlet, useLocation, useNavigate } from "react-router-dom";
 const CommonLayout = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const currentPath = location.pathname.split("/").at(-1) || "";
+  const currentPath = location.pathname.split("/")[1];
+
+  console.log(currentPath);
 
   const tabItems = useMemo(() => {
-    return [
-      {
+    return {
+      main: {
         label: "Main",
-        value: "main",
+        route: "/main",
       },
-      {
+      article: {
         label: "Article",
-        value: "article/features",
+        route: "/article/features",
       },
-      {
+      logout: {
         label: "Logout",
-        value: "logout",
+        route: "/logout",
       },
-    ];
+    };
   }, []);
 
   return (
@@ -29,16 +31,16 @@ const CommonLayout = () => {
       defaultValue={currentPath}
       className='w-full h-full flex'
       orientation='vertical'
-      onValueChange={value => navigate(`/${value}`)}
+      onValueChange={key => navigate(tabItems[key as keyof typeof tabItems].route)}
     >
       <TabsList className='h-full w-40 flex-col gap-3 border-r border-secondary rounded-none bg-white'>
-        {tabItems.map(item => (
+        {Object.keys(tabItems).map(key => (
           <TabsTrigger
-            key={item.label}
-            value={item.value}
+            key={key}
+            value={key}
             className='w-full justify-start data-[state=active]:bg-primary data-[state=active]:text-secondary'
           >
-            {item.label}
+            {tabItems[key as keyof typeof tabItems].label}
           </TabsTrigger>
         ))}
       </TabsList>
