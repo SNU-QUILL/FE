693cf20c89fd590308b20d6ab77154033cd56f06
조병우 <whquddn55@gmail.com>
2024-06-01 21:05:47 +0900

    feature: EditorPick 설정 연결

--- apps/admin/src/pages/main/components/EditorPick.tsx
+++ apps/admin/src/pages/main/components/EditorPick.tsx
@@ -1,14 +1,26 @@
 import { Button } from "@/components/ui/button";
+import TopArticleDialog from "@/pages/main/components/dialog/ArticleTableDialog";
+import { useState } from "react";
 
 const EditorPick = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
+      <TopArticleDialog isOpen={isOpen} onOpenChange={setIsOpen} onSelect={id => alert(id)} />
       <div className='text-primary text-2xl'>Editor&apos;s Picks</div>
       <div className='flex gap-10'>
-        <Button className='h-36 grow hover:animate-pulse'>Edit Editor&apos;s Pick</Button>
-        <Button className='h-36 grow hover:animate-pulse'>Edit Editor&apos;s Pick</Button>
-        <Button className='h-36 grow hover:animate-pulse'>Edit Editor&apos;s Pick</Button>
-        <Button className='h-36 grow hover:animate-pulse'>Edit Editor&apos;s Pick</Button>
+        <Button className='h-36 grow hover:animate-pulse' onClick={() => setIsOpen(true)}>
+          Edit Editor&apos;s Pick
+        </Button>
+        <Button className='h-36 grow hover:animate-pulse' onClick={() => setIsOpen(true)}>
+          Edit Editor&apos;s Pick
+        </Button>
+        <Button className='h-36 grow hover:animate-pulse' onClick={() => setIsOpen(true)}>
+          Edit Editor&apos;s Pick
+        </Button>
+        <Button className='h-36 grow hover:animate-pulse' onClick={() => setIsOpen(true)}>
+          Edit Editor&apos;s Pick
+        </Button>
       </div>
     </div>
   );
