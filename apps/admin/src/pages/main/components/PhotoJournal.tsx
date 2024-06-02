600c53c8d147841e82d00ffd653b86264d973e71
조병우 <whquddn55@gmail.com>
2024-06-02 10:52:52 +0900

    feature: 포토저널 추가 form 추가

--- apps/admin/src/pages/main/components/PhotoJournal.tsx
+++ apps/admin/src/pages/main/components/PhotoJournal.tsx
@@ -1,16 +1,72 @@
-import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const PhotoJournal = () => {
+  const [api, setApi] = useState<CarouselApi>();
+
+  console.log(api?.canScrollNext());
   return (
     <div>
       <div className='text-primary text-2xl'>Photo Journals</div>
-      <Button
-        variant='secondary'
-        className='h-96 w-full outline-dashed outline-primary hover:animate-pulse hover:bg-primary/30'
-      >
-        Edit PhotoJournals
-      </Button>
+      <div className='mx-10'>
+        <Carousel setApi={setApi}>
+          <CarouselContent>
+            <CarouselItem>
+              <PhotoJournalItem id='journal-1' />
+            </CarouselItem>
+            <CarouselItem>
+              <PhotoJournalItem id='journal-2' />
+            </CarouselItem>
+            <CarouselItem>
+              <PhotoJournalItem id='journal-3' />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   );
 };
+
+const PhotoJournalItem = ({ id }: { id: string }) => {
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className='relative h-full'>
+      <Input type='file' id={id} className='hidden' accept='image/*' onChange={handleFileChange} />
+      <label
+        htmlFor={id}
+        className='flex items-center justify-center text-sm h-96 border-[3.5px] border-dashed border-primary bg-secondary hover:animate-pulse hover:bg-primary/30 hover:cursor-pointer rounded-md'
+      >
+        {preview && (
+          <img src={preview} alt='Preview' className='w-full h-full object-cover rounded-md' />
+        )}
+        <div className='absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center'>
+          <div>Edit photo journal</div>
+        </div>
+      </label>
+    </div>
+  );
+};
+
 export default PhotoJournal;
