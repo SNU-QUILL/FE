8b458f8acee8c9c313c35a60414bb458e98390fe
조병우 <whquddn55@gmail.com>
2024-07-02 13:48:47 +0900

    feat: 메거진 추가 디자인 수정

--- apps/admin/src/pages/main/components/Magazine.tsx
+++ apps/admin/src/pages/main/components/Magazine.tsx
@@ -9,7 +9,7 @@ const Magazine = () => {
   };
 
   return (
-    <div className='w-full'>
+    <div className='w-full flex gap-4 h-[380px]'>
       <Input
         type='file'
         id='magazine'
@@ -19,11 +19,21 @@ const Magazine = () => {
       />
       <label
         htmlFor='magazine'
-        className='flex items-center justify-center w-full h-96 outline-dashed outline-primary bg-secondary hover:animate-pulse hover:bg-primary/30 hover:cursor-pointer rounded-md'
+        className='flex items-center justify-center grow outline-dashed outline-primary bg-secondary hover:animate-pulse hover:bg-primary/30 hover:cursor-pointer rounded-md'
       >
         <div>Add magazine</div>
       </label>
+      <MagazineItem />
+      <MagazineItem />
+      <MagazineItem />
     </div>
   );
 };
+
+const MagazineItem = () => {
+  return (
+    <div className='w-[280px] shrink-0 flex items-center justify-center border'>MagazineItem</div>
+  );
+};
+
 export default Magazine;
