b435cb590d194cb009a61c955022ccce38b4ed67
조병우 <whquddn55@gmail.com>
2024-09-26 21:27:20 +0900

    fix: TopArticle 섹션 디자인 수정

--- apps/admin/src/pages/main/components/TopArticle.tsx
+++ apps/admin/src/pages/main/components/TopArticle.tsx
@@ -13,18 +13,18 @@ const TopArticle = () => {
   };
 
   return (
-    <div className='flex h-60'>
+    <div className='flex h-[421px]'>
       <Button
         variant='secondary'
-        className='basis-2/3 grow h-full outline-dashed outline-primary hover:animate-pulse hover:bg-primary/30'
+        className='basis-2/3 grow h-full w-[738px] outline-dashed outline-primary hover:animate-pulse hover:bg-primary/30'
         onClick={openTopArticleDialog}
       >
         Edit TopArticle
       </Button>
       <div className='basis-1/3 shrink-0'>
-        <div className='h-16 flex justify-center items-center'>Recent Opinion1</div>
-        <div className='h-16 flex justify-center items-center'>Recent Opinion2</div>
-        <div className='h-16 flex justify-center items-center'>Recent Opinion3</div>
+        <div className='h-1/3 flex justify-center items-center'>Recent Opinion1</div>
+        <div className='h-1/3 flex justify-center items-center'>Recent Opinion2</div>
+        <div className='h-1/3 flex justify-center items-center'>Recent Opinion3</div>
       </div>
     </div>
   );
