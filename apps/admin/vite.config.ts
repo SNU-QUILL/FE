ec0909be39321e9e32e3a3c6bf04a7615d587f5f
조병우 <whquddn55@gmail.com>
2024-09-26 10:01:18 +0900

    fix: proxy url 변경

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://snuquill.com",
         changeOrigin: true,
       },
     },
