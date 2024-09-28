2b2683b121fba2ac4a0a85b9b2f0e988d12e2e35
조병우 <whquddn55@gmail.com>
2024-09-28 20:49:45 +0900

    fix: tailwindcss 적용 안 되는 문제 해결

--- apps/admin/tailwind.config.js
+++ apps/admin/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "../../packages/ui/src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
