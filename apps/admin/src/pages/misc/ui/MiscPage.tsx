b297d142e95109527ae36d0ccba9574a183a6f02
조병우 <whquddn55@gmail.com>
2024-10-04 16:15:46 +0900

    fix: MiscPage 디자인 변경

--- apps/admin/src/pages/misc/ui/MiscPage.tsx
+++ apps/admin/src/pages/misc/ui/MiscPage.tsx
@@ -4,7 +4,7 @@ const MiscPage = () => {
   return (
     <div>
       <div className='flex flex-col items-start gap-4'>
-        <h1 className='text-2xl font-bold'>Misc Page</h1>
+        <div className='text-4xl font-bold text-primary'>Misc</div>
         <div className='flex items-center gap-2'>
           <IntroductionButton />
         </div>
