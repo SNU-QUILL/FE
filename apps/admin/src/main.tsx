4bcb10bcda3410a416c826bc4d0f5a21759d27f9
조병우 <whquddn55@gmail.com>
2024-10-10 23:20:03 +0900

    fix: build error

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { setupWorker } from "msw/browser";
-import { handlers } from "@/shared/mocks/handler";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import GlobalConfirm from "@/app/modal/GlobalConfirm";
 import GlobalDialog from "@/app/modal/GlobalDialog";
 import { Toaster } from "@repo/ui";
 import AppRouter from "@/app/routes/AppRouter";
 
-const worker = setupWorker();
 if (import.meta.env.MODE === "development") {
-  // worker.use(...handlers);
-  // worker.start({ onUnhandledRequest: "bypass" });
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
