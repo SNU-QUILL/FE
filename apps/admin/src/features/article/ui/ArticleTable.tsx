71ea386c0412b51a4f8b9d5c821ffb8f79832b95
조병우 <whquddn55@gmail.com>
2024-10-21 22:48:16 +0900

    fix: toggleVisible confirm 창 confirm시 닫히도록 수정

--- apps/admin/src/features/article/ui/ArticleTable.tsx
+++ apps/admin/src/features/article/ui/ArticleTable.tsx
@@ -52,6 +52,7 @@ const ArticleTable = (props: IArticleTableProps) => {
           id,
           invisible,
         });
+        closeConfirmDialog();
         props.onArticleSave?.();
       },
     });
