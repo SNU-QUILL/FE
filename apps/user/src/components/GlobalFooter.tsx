99432c704bfba01cacf6cce5ccce454b709eb895
조병우 <whquddn55@gmail.com>
2024-11-25 20:46:37 +0900

    fix: Footer css 변경

--- apps/user/src/components/GlobalFooter.tsx
+++ apps/user/src/components/GlobalFooter.tsx
@@ -12,27 +12,27 @@ const GlobalFooter = () => {
     <footer className='w-full bg-primary flex flex-col items-center text-white font-light text-lg'>
       <div className='grid grid-cols-3 w-full py-10'>
         <div className='flex justify-center gap-20'>
-          <div className='font-medium'>About SNU QUILL</div>
-          <div>
-            <div>Who We Are</div>
-            <div>Our Mission</div>
-            <div>Meet the Staff</div>
+          <p className='font-medium'>About SNU QUILL</p>
+          <div className='font-light'>
+            <p>Who We Are</p>
+            <p>Our Mission</p>
+            <p>Meet the Staff</p>
           </div>
         </div>
 
         <div className='flex justify-center gap-20'>
-          <div className='font-medium'>Contact Us</div>
-          <div>
-            <div>Feedback & Complains</div>
-            <div>Advertise with Us</div>
-            <div>Letter to the Editor</div>
+          <p className='font-medium'>Contact Us</p>
+          <div className='font-light'>
+            <p>Feedback & Complains</p>
+            <p>Advertise with Us</p>
+            <p>Letter to the Editor</p>
           </div>
         </div>
-        <div className='whitespace-pre-wrap'>
+        <p className='font-light whitespace-pre-wrap'>
           {
             "Gwanak-ro, Gwanak-gu, Seoul\nCopyright 2023 The SNU QUILL.\nAll rights reserved.\nmail to snuquill@gmail.com"
           }
-        </div>
+        </p>
       </div>
       <Button
         variant='outline'
