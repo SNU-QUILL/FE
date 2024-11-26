550ef46728ffaa607147f549bbd5c65edfa6821b
thuthi <whquddn55@gmail.com>
2024-11-27 00:01:47 +0900

    fix: msw 항상 실행되도록 변경

--- apps/user/src/main.tsx
+++ apps/user/src/main.tsx
@@ -7,10 +7,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-if (import.meta.env.MODE === "development") {
-  const { worker } = await import("@/mocks/browser");
-  await worker.start();
-}
+const { worker } = await import("@/mocks/browser");
+worker.start();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
