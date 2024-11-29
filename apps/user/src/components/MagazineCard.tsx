49762f60795c19512588ada0c21dbe205a61f8b5
thuthi <whquddn55@gmail.com>
2024-11-29 15:19:53 +0900

    fix: Magazine 디자인 수정 일부 원복(메인 페이지에서 깨짐)

--- apps/user/src/components/MagazineCard.tsx
+++ apps/user/src/components/MagazineCard.tsx
@@ -9,25 +9,20 @@ interface IMagazineCardProps {
 
 const MagazineCardSkeleton = () => (
   <div className='flex flex-col items-center gap-1'>
-    <Skeleton className='w-full h-[120px] md:h-[150px] md:w-[120px] xl:h-[350px] xl:w-[240px]' />
+    <Skeleton className='h-[120px] w-full md:h-[150px] md:w-[120px] xl:h-[350px] xl:w-[240px]' />
     <Skeleton className='w-14 h-4' />
   </div>
 );
 
 const MagazineCard = ({ volumeNumber, volumeCoverLink, fileLink }: IMagazineCardProps) => {
   return (
-    <div key={volumeNumber} className='flex flex-col items-center gap-1'>
-      <div className='h-[120px] md:h-[150px] md:w-[120px] xl:h-[350px] xl:w-[240px]'>
-        <a key={volumeNumber} href={fileLink} target='_blank' rel='noreferrer'>
-          <SkeletonImage
-            src={volumeCoverLink}
-            alt={`Volume ${volumeNumber}`}
-            className='w-full h-full hover:animate-hover-scale'
-          />
-        </a>
-      </div>
-      <p className='text-center text-sm text-gray-500'>VOL.{volumeNumber}</p>
-    </div>
+    <a key={volumeNumber} href={fileLink} target='_blank' rel='noreferrer'>
+      <SkeletonImage
+        src={volumeCoverLink}
+        alt={`Volume ${volumeNumber}`}
+        className='w-full h-full hover:animate-hover-scale'
+      />
+    </a>
   );
 };
 MagazineCard.Skeleton = MagazineCardSkeleton;
--- apps/user/src/pages/archives/ArchivePage.tsx
+++ apps/user/src/pages/archives/ArchivePage.tsx
@@ -22,7 +22,14 @@ const ArchivePage = () => {
         <div className='text-primary text-[25px] font-medium'>Issue</div>
         <div className='grid grid-cols-4 gap-x-4 gap-y-4 m-[10px_20px]'>
           {magazines
-            ? magazines.map(magazine => <MagazineCard key={magazine.volumeNumber} {...magazine} />)
+            ? magazines.map(magazine => (
+                <div key={magazine.volumeNumber} className='flex flex-col items-center gap-1'>
+                  <div className='h-[120px] md:h-[150px] md:w-[120px] xl:h-[350px] xl:w-[240px]'>
+                    <MagazineCard {...magazine} />
+                  </div>
+                  <p className='text-center text-sm text-gray-500'>VOL.{magazine.volumeNumber}</p>
+                </div>
+              ))
             : Array.from({ length: PAGE_SIZE }).map((_, index) => (
                 <MagazineCard.Skeleton key={index} />
               ))}
